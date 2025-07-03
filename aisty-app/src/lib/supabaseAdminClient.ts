05e9e04ddd5fc4845b50c2e3a0c14ace02a93fa1
りおん@象徴-メッシ <h.rion.0910@gmail.com>
2025-07-03 11:34:13 +0900

    supabaseAdminClient.tsの追加

---
+++ aisty-app/src/lib/supabaseAdminClient.ts
@@ -0,0 +1,8 @@
+// src/lib/supabaseAdminClient.ts
+import { createClient } from '@supabase/supabase-js';
+
+export const supabaseAdmin = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.SUPABASE_SERVICE_ROLE_KEY!, // サーバー専用キー
+  { auth: { persistSession: false } }
+);
