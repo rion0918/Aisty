f75e0cda5024c8944e7c8b7e3808e2ea22e61407
りおん@象徴-メッシ <h.rion.0910@gmail.com>
2025-09-02 13:00:28 +0900

    デプロイ用の修正をした

--- aisty-app/src/middleware.ts
+++ aisty-app/src/middleware.ts
@@ -26,8 +26,8 @@ export default clerkMiddleware(async (auth, req) => {
 
 export const config = {
   matcher: [
-    '/((?!_next|[^?]*\\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    '/((?!.+\\.[\\w]+$|_next).*)',
+    '/',
     '/(api|trpc)(.*)',
   ],
 };
-
